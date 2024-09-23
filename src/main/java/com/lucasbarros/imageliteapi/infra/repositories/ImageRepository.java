83848c1b010f2470be1c1b849b9dbe4522be0eeb
lbc354 <lbarroscarregozi@gmail.com>
2024-09-23 19:01:19 -0300

    Adicionando capacidade de executar Specifications no Repositório de imagens

--- src/main/java/com/lucasbarros/imageliteapi/infra/repositories/ImageRepository.java
+++ src/main/java/com/lucasbarros/imageliteapi/infra/repositories/ImageRepository.java
@@ -1,9 +1,20 @@
 package com.lucasbarros.imageliteapi.infra.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.lucasbarros.imageliteapi.domain.entities.Image;
+import com.lucasbarros.imageliteapi.domain.enums.ImageExtension;
 
-public interface ImageRepository extends JpaRepository<Image, String> {
+public interface ImageRepository extends JpaRepository<Image, String>, JpaSpecificationExecutor<Image> {
+	
+	default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query) {
+		if (extension != null) {
+			// add in query
+		}
+		return findAll();
+	}
 
 }
