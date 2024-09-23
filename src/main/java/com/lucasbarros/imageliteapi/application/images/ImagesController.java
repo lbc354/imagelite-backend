15c401fc678bb1ce927a447c8fe17598f6322ef0
lbc354 <lbarroscarregozi@gmail.com>
2024-09-23 16:28:26 -0300

    Salvando a imagem através da API

--- src/main/java/com/lucasbarros/imageliteapi/application/images/ImagesController.java
+++ src/main/java/com/lucasbarros/imageliteapi/application/images/ImagesController.java
@@ -1,7 +1,9 @@
 package com.lucasbarros.imageliteapi.application.images;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,21 +11,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.lucasbarros.imageliteapi.domain.entities.Image;
+import com.lucasbarros.imageliteapi.domain.enums.ImageExtension;
+import com.lucasbarros.imageliteapi.domain.services.ImageService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 // todo controlador rest precisa dessa annotation para que ele receba requisições http
 @RestController
 // para dizer qual url ele vai ficar escutando
 @RequestMapping("/v1/images")
-// para os logs
-@Slf4j
+// lombok
+@Slf4j // para os logs
+@RequiredArgsConstructor
 public class ImagesController {
 	
-	// método que vai receber o upload de uma imagem
+	private final ImageService imgserv;
+	// o spring vai pegar uma implementação dessa interface (em ImageServiceImpl), vai instanciar um objeto e vai injetar aqui para utilizarmos
+	
 	// formato utilizado por formulários com mídia: multipart/form-data
-	// @RequestBody: É usada para mapear o corpo da requisição HTTP para um objeto em Java. É normalmente utilizada para lidar com dados em formato JSON, XML ou outros tipos de conteúdo enviado no corpo da requisição.
-	// @RequestParam: É usada para extrair parâmetros da URL ou da query string (parte da URL após o ?). Esses parâmetros geralmente estão no formato de dados de formulário ou são valores simples, como números, strings, etc.
-	// Portanto, a anotação @RequestParam não é limitada a qualquer formato específico, mas é usada para capturar parâmetros de URL, enquanto @RequestBody lida com o corpo da requisição, tipicamente em JSON ou outro formato similar.
 	// @RequestParam(value = "file", required = false) MultipartFile file -> parâmetro opcional
 	
 	@PostMapping
@@ -31,11 +38,24 @@ public class ImagesController {
 			@RequestParam("file") MultipartFile file,
 			@RequestParam("name") String name,
 			@RequestParam("tags") List<String> tags
-			) {
+			) throws IOException {
 
 		log.info("Imagem recebida. name: {}, size: {}", file.getOriginalFilename(), file.getSize());
-		log.info("Nome dado para a imagem: {}", name);
-		log.info("Tags: {}", tags);
+		
+		Image image = Image.builder()
+				.name(name)
+				.tags(String.join(";", tags)) // ["tag1","tag2"] -> "tag1,tag2"
+				.size(file.getSize())
+				
+				.extension(ImageExtension.valorDe(
+						MediaType.valueOf(file.getContentType())
+						))
+				
+				.file(file.getBytes())
+				.build();
+		
+		imgserv.save(image);
+		
 		return ResponseEntity.ok().build();
 
 	}
