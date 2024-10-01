35ef405aaa58725d4ced9e18598f6a958a97ddd8
lbc354 <lbarroscarregozi@gmail.com>
2024-10-01 18:52:21 -0300

    Criando método para obter o email através do Token no back-end

--- src/main/java/com/lucasbarros/imageliteapi/application/jwt/JwtService.java
+++ src/main/java/com/lucasbarros/imageliteapi/application/jwt/JwtService.java
@@ -11,6 +11,10 @@ import org.springframework.stereotype.Service;
 import com.lucasbarros.imageliteapi.domain.AccessToken;
 import com.lucasbarros.imageliteapi.domain.entities.User;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
 
@@ -28,21 +32,40 @@ public class JwtService {
 				.expiration(expirationDate())
 				.claims(tokenClaims(user))
 				.compact();
-
 		return new AccessToken(token);
 	}
-	
+
 	private Date expirationDate() {
 		var expirationMinutes = 60;
 		LocalDateTime deadline = LocalDateTime.now().plusMinutes(expirationMinutes);
 		return Date.from(deadline.atZone(ZoneId.systemDefault()).toInstant());
 	}
-	
+
 	// informações para o front-end como nome, etc. ("olá fulano")
 	private Map<String, Object> tokenClaims(User user) {
 		Map<String, Object> claims = new HashMap<>();
 		claims.put("username", user.getUsername());
 		return claims;
+	}
+
+	public String getEmailFromToken(String tokenJwt) {
+		try {
+		
+			JwtParser build = Jwts.parser().verifyWith(secretKeyGenerator.getKey()).build();
+		
+			Jws<Claims> jwsClaims = build.parseSignedClaims(tokenJwt);
+			// no momento do parse (decodificar token),
+			// se o token estiver expirado ele vai lançar uma exception (JwtException),
+			// ou se o token não for um jwt, se for uma string qualquer,
+			// ele também lança exception
+			
+			Claims claims = jwsClaims.getPayload();
+			
+			return claims.getSubject();
+		
+		} catch (JwtException e) {
+			throw new InvalidTokenException(e.getMessage());
+		}
 	}
 
 }
